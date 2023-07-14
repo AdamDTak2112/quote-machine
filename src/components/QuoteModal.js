3799bf21b8e480985e79acff0ea6ca4854da4c28
Adam Tak <adam.tak2112@gmail.com>
2023-07-14 14:58:42 -0600

    create quote modal and button

---
+++ src/components/QuoteModal.js
@@ -0,0 +1,12 @@
+import Card from 'react-bootstrap/Card';
+
+export default function QuoteModal () {
+    return(
+        <div id="quote-box">
+            <Card>
+                <Card.Header>Quote</Card.Header>
+            </Card>
+        </div>
+    );
+
+}
