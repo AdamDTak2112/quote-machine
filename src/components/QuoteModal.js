d00237232a1adff5e0b342c46590816314079481
Adam Tak <adam.tak2112@gmail.com>
2023-07-14 15:30:49 -0600

    general layout done

--- src/components/QuoteModal.js
+++ src/components/QuoteModal.js
@@ -2,11 +2,23 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 export default function QuoteModal () {
+
+    
     return(
         <div id="quote-box">
-            <Card>
-                <Card.Header>Quote</Card.Header>
+            <Card className='text-center'>
+                <Card.Header>Quote Generator</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote">
+                        <p id="text">QUOTE GOES HERE</p>
+                        <footer className="blockquote-footer" id="author">
+                            Author here
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+                <Button>New Quote</Button>
             </Card>
+            
         </div>
     );
 
